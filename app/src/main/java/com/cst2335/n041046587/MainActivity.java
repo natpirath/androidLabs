f0f09b6fab526e6dc75e4d51619f15a0714109f2
natpirath <natpirath@gmail.com>
2022-06-10 22:48:03 -0400

    Finished Lab 3

--- app/src/main/java/com/cst2335/n041046587/MainActivity.java
+++ app/src/main/java/com/cst2335/n041046587/MainActivity.java
@@ -1,55 +1,68 @@
 package com.cst2335.n041046587;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.LinearLayout;
-import android.widget.Switch;
-import android.widget.Toast;
+
+import android.widget.EditText;
+import android.widget.ImageButton;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.snackbar.BaseTransientBottomBar;
-import com.google.android.material.snackbar.Snackbar;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    LinearLayout MainLayout;
+    private EditText editTextViewEmail;
+    private ImageButton ButtonLogin;
+
+    public static final String SP = "Shared Preferences";
+    public static final String EMAIL = "email";
+    private String email;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main_lab3);
+
+        editTextViewEmail = (EditText) findViewById(R.id.editTextEmail);
+        ButtonLogin = findViewById(R.id.button);
+
+        /*
+         * load user's email address from SharedPreferences
+         */
+        SharedPreferences prefs = getSharedPreferences(SP, MODE_PRIVATE);
+        email = prefs.getString(EMAIL, "");
+
+        /*
+         * Set email address of Email Edit Text
+         */
+        editTextViewEmail.setText(email);
 
 
         /*
          * Give a message when clicking the Click Here button
          */
-        Button btn = (Button)findViewById(R.id.button1);
-        btn.setOnClickListener(v -> {
-
-            Toast t = Toast.makeText(getApplicationContext(),
-                    getResources().getString(R.string.toast_message),
-                    Toast.LENGTH_LONG);
-            t.show();
+        ButtonLogin.setOnClickListener(view -> {
+            Intent goToProfile = new Intent(MainActivity.this, ProfileActivity.class);
+            goToProfile.putExtra("email", editTextViewEmail.getText().toString());
+            startActivity(goToProfile);
         });
 
-        /*
-         * Give a message when switching the switch button.
-         */
-        Switch s = (Switch) findViewById(R.id.s);
-        s.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                s.setChecked(isChecked);
-                if(s.isChecked()){
-                    Snackbar.make(s, getResources().getString(R.string.s_on), BaseTransientBottomBar.LENGTH_SHORT).setAction("Undo",
-                            click -> buttonView.setChecked(!isChecked)).show();
-                }
-                else {
-                    Snackbar.make(s, getResources().getString(R.string.s_off), BaseTransientBottomBar.LENGTH_SHORT).setAction("Undo",
-                            click -> buttonView.setChecked(!isChecked)).show();
-                }
-            }
-        });
-    }
+        }
+
+        @Override
+        protected void onPause() {
+            super.onPause();
+
+            // save a user's email address
+            SharedPreferences sharedPreferences = getSharedPreferences(SP, MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString(EMAIL, editTextViewEmail.getText().toString());
+
+            editor.apply();
+        }
+
+
 }
---
+++ app/src/main/java/com/cst2335/n041046587/ProfileActivity.java
@@ -0,0 +1,99 @@
+package com.cst2335.n041046587;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private EditText editTextName;
+    private EditText editTextEmail;
+    private ImageButton button;
+
+    public static final String TAG = "PROFILE_ACTIVITY";
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        Log.e(TAG, " in function: " + "onCreate");
+
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        button = (ImageButton) findViewById(R.id.button);
+
+        // get intent from MainActivity
+        Intent fromMain = getIntent();
+        editTextEmail.setText(fromMain.getStringExtra("email"));
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+
+    ActivityResultLauncher<Intent> myPictureTakerLauncher;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null)
+            myPictureTakerLauncher.launch(takePictureIntent);
+        }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.e(TAG, " in function: " + "onActivityResult");
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            button.setImageBitmap(imageBitmap);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(TAG, " in function: " + "onStart");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(TAG, " in function: " + "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG, " in function: " + "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(TAG, " in function: " + "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG, " in function: " + "onResume");
+    }
+
+}
